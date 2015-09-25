8ca81c966e3541afaccd0de792003ae797ccff90
Jafnee <jafneejesmee@gmail.com>
2015-09-25 15:15:24 +0800

    release key downloaded to tmp instead

--- install_scripts/custom-gnome.sh
+++ install_scripts/custom-gnome.sh
@@ -4,8 +4,8 @@ apt-get install gnome-tweak-tool
 
 # arc theme https://github.com/horst3180/Arc-theme
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
-sudo apt-key add - < Release.key  
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key -O /tmp/Release.key
+sudo apt-key add - < /tmp/Release.key
 sudo apt-get update
 sudo apt-get install arc-theme
 
