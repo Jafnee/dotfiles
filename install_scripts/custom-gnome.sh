ca7e1f0146902fd5166307e8846edb55618db517
Jafnee <jafneejesmee@gmail.com>
2015-09-25 15:05:43 +0800

    add missing -y for paper-icon-theme add repo key for arc-theme

--- install_scripts/custom-gnome.sh
+++ install_scripts/custom-gnome.sh
@@ -4,9 +4,11 @@ apt-get install gnome-tweak-tool
 
 # arc theme https://github.com/horst3180/Arc-theme
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
+wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_15.04/Release.key
+sudo apt-key add - < Release.key  
 sudo apt-get update
 sudo apt-get install arc-theme
 
 # paper icons 
 sudo add-apt-repository -y ppa:snwh/pulp
-sudo apt-get update && sudo apt-get install paper-icon-theme
+sudo apt-get update && sudo apt-get install -y paper-icon-theme
