235f15c2055b78220bde9e5a49ecbe626ac9f000
Jafnee <jafneejesmee@gmail.com>
2015-09-25 18:09:07 +0800

    bring over files from backup branch

---
+++ gimme.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import os
+from os.path import join as joinpth
+import shutil
+
+import config as cfg
+
+DEBUG=True
+
+def backup(imp=False):
+	if imp:
+		print("Importing...")
+		dst = cfg.sdir
+	else:
+		print("Backing up...")
+		dst = cfg.bdir
+		try:
+			os.makedirs(dst)
+		except FileExistsError:
+			pass
+	for item in cfg.items:
+		print("  "+item)
+		src = joinpth(cfg.dir, item)
+
+		try:
+			shutil.copy2(src, dst)
+		except IsADirectoryError:
+			dst = joinpth(dst, item)
+			try:
+				shutil.copytree(src, dst)
+			except FileExistsError:
+				shutil.rmtree(dst)
+				shutil.copytree(src, dst)
+		except shutil.SameFileError:
+			pass
+	print("Done!")
+
+def import_():
+	backup(imp=True)
+
+def symlink():
+	print("Linking...")
+	for item in cfg.items:
+		print("  "+item)
+		src = joinpth(cfg.sdir, item)
+		dst = joinpth(cfg.dir, item)
+		try:
+			os.symlink(src,dst)
+		except FileExistsError:
+			try:
+				os.remove(dst)
+			except IsADirectoryError:
+				shutil.rmtree(dst)
+			os.symlink(src,dst)
+	print("Done!")
+
+if __name__ == '__main__':
+	# main()
+	backup()
+	# import_()
+	# symlink()
